93bf15cd584a6cacb626e9e23d3c6174637cb1e8
kouodziey <kouodziey@186253f6-d097-5331-538c-8b5206fdb8b3>
2011-01-06 00:03:26 +0000

    [android] Ekran glowny, klasy uzytkowe

--- impl/android/src/pl/multitalk/android/StartActivity.java
+++ impl/android/src/pl/multitalk/android/StartActivity.java
@@ -1,7 +1,13 @@
 package pl.multitalk.android;
 
+import java.io.UnsupportedEncodingException;
+
+import pl.multitalk.android.util.DigestUtil;
+import pl.multitalk.android.util.NetworkUtil;
+import pl.multitalk.android.util.NetworkUtil.WifiNotEnabledException;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Ekran startowy aplikacji
@@ -9,9 +15,35 @@ import android.os.Bundle;
  */
 public class StartActivity extends Activity {
 
+    String username = "kouodziey";
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_activity);
+        
+        // debug
+        printDebugInfo();
+    }
+    
+    
+    
+    /**
+     * Wpisuje do logów informacje na potrzeby debugu
+     */
+    private void printDebugInfo(){
+        boolean wifiOn = NetworkUtil.isWifiEnabled(getBaseContext());
+        Log.d("Multitalk-DEBUG", "Wifi enabled: " + wifiOn);
+        
+        try {
+            String mac = NetworkUtil.getWifiMAC(getBaseContext());
+            Log.d("Multitalk-DEBUG", "MAC address: " + mac);
+            
+            String ipAddress = NetworkUtil.getIPaddressAsString(getBaseContext());
+            Log.d("Multitalk-DEBUG", "IP address: " + ipAddress);
+        
+        } catch (WifiNotEnabledException e) {
+            Log.d("Multitalk-DEBUG", "WifiNotEnabledException occured");
+        }
     }
 }
