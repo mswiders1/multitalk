97c460f8816155ea9cc0ea537a4ae514203219d5
wujo008 <wujo008@186253f6-d097-5331-538c-8b5206fdb8b3>
2011-01-10 23:02:25 +0000

    GUI: user settings & delete contacts

--- impl/java/src/Test1.java
+++ impl/java/src/Test1.java
@@ -12,9 +12,10 @@ public class Test1 {
 		// TODO Auto-generated method stub
 		Vector <Contact> v = new Vector<Contact>();
 		v.add(new Contact(1,"krzysiek",true));
-		v.add(new Contact(2,"krzysiek2",true));		
+		v.add(new Contact(2,"krzysiek2",true));
+		Contact c =new Contact(3,"ja",true);
 		MyId myid = new MyId();
-		GUI Gui = new GUI(v);
+		GUI Gui = new GUI(v,c);
 		
 		//myid.findNIC();
 		//System.out.println(myid.findIp(myid.inaddr));
