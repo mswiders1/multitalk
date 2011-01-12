51dc28c4685158c533b65c7003238661fd2660f3
soldier.kam <soldier.kam@186253f6-d097-5331-538c-8b5206fdb8b3>
2011-01-12 21:20:27 +0000

    Skrypt do uruchamiania v2

--- trunk/impl/python/run.sh
+++ trunk/impl/python/run.sh
@@ -3,10 +3,10 @@
 PYTHON=`type -P python`
 PYTHON2=`type -P python2`
 
-if [ x$PYTHON2 == x"" ] ; then
+if [ "x$PYTHON2" == "x" ] ; then
     echo "Run by default python version"
-    exec $PYTHON main.py
+    exec "$PYTHON" main.py
 else
     echo "Run by Python 2.x"
-    exec $PYTHON2 main.py
+    exec "$PYTHON2" main.py
 fi;
