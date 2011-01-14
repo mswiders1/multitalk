15604b8aa7b4fb3c2df111e1fa4ab4d968f8aedb
soldier.kam <soldier.kam@186253f6-d097-5331-538c-8b5206fdb8b3>
2011-01-14 11:55:55 +0000

    Python: poprawka skryptu do uruchamiania

--- trunk/impl/python/run.sh
+++ trunk/impl/python/run.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 PYTHON=`type -P python`
 PYTHON2=`type -P python2`
 
 if [ "x$PYTHON2" == "x" ] ; then
-    echo "Run by default python version"
-    exec "$PYTHON" main.py
+    echo "Run by default python version";
+    "$PYTHON" main.py;
 else
-    echo "Run by Python 2.x"
-    exec "$PYTHON2" main.py
+    echo "Run by Python 2.x";
+    "$PYTHON2" main.py;
 fi;
