297586a50ceaebb95b72bce9bc9f18b22eb31d75
soldier.kam <soldier.kam@186253f6-d097-5331-538c-8b5206fdb8b3>
2011-01-14 17:49:38 +0000

    Python: skrypt do budowania versja 1.0

---
+++ trunk/impl/python/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Kompilacja zasobów"
+pyrcc4 ./qtForms/res.qrc -o ./qtForms/res_rc.py
+echo "Kompilacja interfejsu użytkownika"
+pyuic4 ./qtForms/settings.ui -o ./qtForms/Ui_settings.py
+pyuic4 ./qtForms/test.ui -o ./qtForms/Ui_test.py
+pyuic4 ./qtForms/window.ui -o ./qtForms/Ui_windows.py
+echo "Koniec"
+
