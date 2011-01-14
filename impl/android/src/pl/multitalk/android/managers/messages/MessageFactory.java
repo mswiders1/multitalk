0115253f27e4d132c59f2875ff6626d4815c82e6
kouodziey <kouodziey@186253f6-d097-5331-538c-8b5206fdb8b3>
2011-01-14 01:22:01 +0000

    [android] Operacje sieciowe i komunikaty

---
+++ impl/android/src/pl/multitalk/android/managers/messages/MessageFactory.java
@@ -0,0 +1,19 @@
+package pl.multitalk.android.managers.messages;
+
+/**
+ * Fabryka komunikatów
+ * @author Michał Kołodziejski
+ */
+public class MessageFactory {
+
+    
+    /**
+     * Tworzy klasę komunikatu odpowiedniego typu na podstawie wiadomości w postaci JSON
+     * @param jsonString komunikat w postaci JSON
+     * @return klasa komunikatu odpowiedniego typu
+     */
+    public static Message fromJSON(String jsonString){
+        // TODO
+        return null;
+    }
+}
