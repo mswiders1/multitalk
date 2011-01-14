33b73281168a258a3377a103bf8f51e744d76258
kouodziey <kouodziey@186253f6-d097-5331-538c-8b5206fdb8b3>
2011-01-14 18:27:14 +0000

    [android] Operacje sieciowe

--- trunk/impl/android/src/pl/multitalk/android/managers/messages/Message.java
+++ trunk/impl/android/src/pl/multitalk/android/managers/messages/Message.java
@@ -1,11 +1,37 @@
 package pl.multitalk.android.managers.messages;
 
+import pl.multitalk.android.datatypes.UserInfo;
+
 /**
  * Wiadomość wysyłana/przesyłana do/od klienta
  * @author Michał Kołodziejski
  */
 public interface Message {
 
+    /**
+     * Ustawia dane nadawcy wiadomości
+     * @param senderInfo dane nadawcy wiadomości
+     */
+    void setSenderInfo(UserInfo senderInfo);
+
+    /**
+     * Zwraca dane nadawcy wiadomości
+     * @return dane nadawcy wiadomości
+     */
+    UserInfo getSenderInfo();
+    
+    /**
+     * Ustawia dane odbiorcy wiadomości
+     * @param recipientInfo dane odbiorcy wiadomości
+     */
+    void setRecipientInfo(UserInfo recipientInfo);
+    
+    /**
+     * Zwraca dane odbiorcy wiadomości
+     * @return dane odbiorcy wiadomości
+     */
+    UserInfo getRecipientInfo();
+    
     /**
      * Serializuje do postaci JSON
      * @return zserializowany komunikat
