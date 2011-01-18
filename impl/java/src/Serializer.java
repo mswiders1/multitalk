2ffbb54765beeaa4a7f89fd47493dff86ed5177d
wujo008 <wujo008@186253f6-d097-5331-538c-8b5206fdb8b3>
2011-01-18 03:14:11 +0000

    ide spac

--- impl/java/src/Serializer.java
+++ impl/java/src/Serializer.java
@@ -1,146 +1,260 @@
-import net.sf.json.*;
+import java.lang.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.json.JSONArray;
+
+import net.sf.json.JSONObject;
+
+
+
 import java.util.*;
 
 
 public class Serializer {
 	private JSONObject obj;
+	private Message msg;
 	
 	String pack(Message msg)
 	{
-		String type = msg.getType();
-		if (type.toUpperCase()=="P2P")
-		{
-			obj.put("TYPE", "P2P" );
-			obj.put("CONTENT","MULTITALK_5387132");
-		}
-		else if (type.toUpperCase()=="HII")
-		{
-			JSONArray jarray = new JSONArray();
-			obj.put("TYPE", "HII");
-			obj.put("UID",((HiiMsg)msg).getUid());
-			obj.put("USERNAME", ((HiiMsg)msg).getUid());
-			
-			Iterator<UserVector>  it = ((HiiMsg)msg).getVector().iterator();
-			UserVector uv;
-			JSONObject uobj;
-			while(it.hasNext())
+		this.msg = msg;
+		try{
+			String type = msg.getType();
+			obj = new JSONObject();
+			if (type.toUpperCase()=="P2P")
 			{
-				
-				uobj = new JSONObject();
-				uv = it.next();
-				uobj.put("IP_ADDRESS", uv.getIp_address());
-				uobj.put("UID", uv.getUid());
-				uobj.put("USERNAME", uv.getUsername());
-				
-				jarray.add(uobj);
+				obj.put("TYPE", "P2P" );
 			}
-			obj.put("VECTOR", jarray);
-			
-		}
-		else if (type.toUpperCase()=="LOG")
-		{
-			obj.put("TYPE", "LOG");
-			obj.put("UID", ((LogMsg)msg).getUid());
-			obj.put("USERNAME", ((LogMsg)msg).getUsename());
-			obj.put("IP_ADDRESS", ((LogMsg)msg).getIp_address());
-		}
-		
-		else if (type.toUpperCase()=="MTX")
-		{
-			obj.put("TYPE", "MTX");
-			JSONArray jmatrix, jrow, jvec;
-			Vector<Knowlage> vk;
-			Iterator<Vector<Knowlage>> it = ((MtxMsg)msg).getMac().iterator();
-			Iterator<Knowlage> itk;
-			Knowlage k;
-			jmatrix = new JSONArray();
-			while (it.hasNext())
+			else if (type.toUpperCase()=="HII")
 			{
-				jrow = new JSONArray();
-				vk = it.next();
-				itk = vk.iterator();
-				while (itk.hasNext())
+				JSONArray jarray = new JSONArray();
+				obj.put("TYPE", "HII");
+				obj.put("UID",((HiiMsg)msg).getUid());
+				obj.put("USERNAME", ((HiiMsg)msg).getUid());
+				
+				Iterator<UserVector>  it = ((HiiMsg)msg).getVector().iterator();
+				UserVector uv;
+				JSONObject uobj;
+				while(it.hasNext())
+				{
+					
+					uobj = new JSONObject();
+					uv = it.next();
+					uobj.put("IP_ADDRESS", uv.getIp_address());
+					uobj.put("UID", uv.getUid());
+					uobj.put("USERNAME", uv.getUsername());
+					
+					jarray.add(uobj);
+				}
+				obj.put("VECTOR", jarray);
+				
+			}
+			else if (type.toUpperCase()=="LOG")
+			{
+				obj.put("TYPE", "LOG");
+				obj.put("UID", ((LogMsg)msg).getUid());
+				obj.put("USERNAME", ((LogMsg)msg).getUsename());
+				obj.put("IP_ADDRESS", ((LogMsg)msg).getIp_address());
+			}
+			
+			else if (type.toUpperCase()=="MTX")
+			{
+				obj.put("TYPE", "MTX");
+				JSONArray jmatrix, jrow, jvec;
+				Vector<Knowlage> vk;
+				Iterator<Vector<Knowlage>> it = ((MtxMsg)msg).getMac().iterator();
+				Iterator<Knowlage> itk;
+				Knowlage k;
+				jmatrix = new JSONArray();
+				while (it.hasNext())
+				{
+					jrow = new JSONArray();
+					vk = it.next();
+					itk = vk.iterator();
+					while (itk.hasNext())
+					{
+						k = itk.next();
+						jrow.add(k.getMsg_no());
+					}				
+					jmatrix.add(jrow);
+				}
+				obj.put("MAC", jmatrix);
+					
+				jvec = new JSONArray();
+				Iterator<UserId> ituid = ((MtxMsg)msg).getVector().iterator();
+				UserId uid;
+				while(ituid.hasNext())
+				{
+					uid = ituid.next();
+					jvec.add(uid.getUser_id());
+				}
+						
+				obj.put("VEC", jvec);
+			}
+			
+			else if (type.toUpperCase()=="MSG")
+			{
+				obj.put("TYPE", "MSG");
+				obj.put("SENDER", ((MsgMsg)msg).getSender());
+				obj.put("RECEIVER", ((MsgMsg)msg).getReceiver());
+				obj.put("MSG_ID", ((MsgMsg)msg).getMsg_id());
+				Iterator<Knowlage> itk = ((MsgMsg)msg).getTime_vec().iterator();
+				Iterator<UserId> itu = ((MsgMsg)msg).getVec().iterator();
+				Knowlage k;
+				UserId uid;
+				JSONArray jarray_time = new JSONArray();
+				JSONArray jarray_user = new JSONArray();
+				while(itk.hasNext())
 				{
 					k = itk.next();
-					jrow.add(k.getMsg_no());
-				}				
-				jmatrix.add(jrow);
+					jarray_time.add(k.getMsg_no());
+				}
+				while(itu.hasNext())
+				{
+					uid = itu.next();
+					jarray_user.add(uid.getUser_id());
+				}
+				obj.put("TIME_VEC", jarray_time);
+				obj.put("VEC", jarray_user);
+				obj.put("CONTENT", ((MsgMsg)msg).getContent());			
 			}
-			obj.put("MAC", jmatrix);
-				
-			jvec = new JSONArray();
-			Iterator<UserId> ituid = ((MtxMsg)msg).getVector().iterator();
-			UserId uid;
-			while(ituid.hasNext())
+			
+			else if (type.toUpperCase()=="LIV")
 			{
-				uid = ituid.next();
-				jvec.add(uid.getUser_id());
+				obj.put("TYPE", "LIV");
+				obj.put("UID", ((LivMsg)msg).getUid());
+				obj.put("IP_ADDRESS", ((LivMsg)msg).getIp_address());
+			}
+			else if(type.toUpperCase() == "GET")
+			{
+				obj.put("TYPE", "GET");
+				obj.put("UID", ((GetMsg)msg).getUid());
+				obj.put("MSG_ID", ((GetMsg)msg).getMsg_id());
+			}
+			else if(type.toUpperCase() == "OUT")
+			{
+				obj.put("TYPE", "OUT");
+				obj.put("UID", ((OutMsg)msg).getUid());
+			}
+			else
+			{
+				System.out.print("Nieznany typ wiadomosci przy pakowaniu do JSON'a");
 			}
 					
-			obj.put("VEC", jvec);
+			return obj.toString();
 		}
 		
-		else if (type.toUpperCase()=="MSG")
-		{
-			obj.put("TYPE", "MSG");
-			obj.put("SENDER", ((MsgMsg)msg).getSender());
-			obj.put("RECEIVER", ((MsgMsg)msg).getReceiver());
-			obj.put("MSG_ID", ((MsgMsg)msg).getMsg_id());
-			Iterator<Knowlage> itk = ((MsgMsg)msg).getTime_vec().iterator();
-			Iterator<UserId> itu = ((MsgMsg)msg).getVec().iterator();
-			Knowlage k;
-			UserId uid;
-			JSONArray jarray_time = new JSONArray();
-			JSONArray jarray_user = new JSONArray();
-			while(itk.hasNext())
-			{
-				k = itk.next();
-				jarray_time.add(k.getMsg_no());
-			}
-			while(itu.hasNext())
-			{
-				uid = itu.next();
-				jarray_user.add(uid.getUser_id());
-			}
-			obj.put("TIME_VEC", jarray_time);
-			obj.put("VEC", jarray_user);
-			obj.put("CONTENT", ((MsgMsg)msg).getContent());			
+		catch (Exception e) {         
+            return "ERROR";
 		}
-		
-		else if (type.toUpperCase()=="LIV")
-		{
-			obj.put("TYPE", "LIV");
-			obj.put("UID", ((LivMsg)msg).getUid());
-			obj.put("IP_ADDRESS", ((LivMsg)msg).getIp_address());
-		}
-		else if(type.toUpperCase() == "GET")
-		{
-			obj.put("TYPE", "GET");
-			obj.put("UID", ((GetMsg)msg).getUid());
-			obj.put("MSG_ID", ((GetMsg)msg).getMsg_id());
-		}
-		else if(type.toUpperCase() == "OUT")
-		{
-			obj.put("TYPE", "OUT");
-			obj.put("UID", ((OutMsg)msg).getUid());
-		}
-		else
-		{
-			System.out.print("Nieznany typ wiadomosci przy pakowaniu do JSON'a");
-		}
-				
-		return obj.toString();		
 	}
 	
 	Message unpack(JSONObject obj)
-	{
-		Message msg = new Message();
-		return msg;
+	{	
+		this.msg = new Message();
+		this.obj = obj;
+		try{
+			this.obj = obj;
+			String type = (String) obj.get("TYPE");
+			System.out.print("type: "+ type);
+			if(type.toUpperCase() == "P2P" )
+			{
+				msg.setType("Ptp");
+				return (P2pMsg) msg;
+			}
+			else if(type == "HII")
+			{
+				((HiiMsg)msg).setType("HII");
+				((HiiMsg)msg).setUid((String)obj.get("UID"));
+				((HiiMsg)msg).setUsername((String)obj.get("USERNAME"));
+				
+				UserVector uv;
+				JSONObject o;
+				JSONArray ja = (JSONArray)obj.get("VECTOR");
+				Vector<UserVector> vuv = new Vector<UserVector>();
+				Iterator<JSONObject> it = ja.iterator();
+				while (it.hasNext())
+				{
+					uv = new UserVector();
+					o = (JSONObject)it.next();
+					uv.setIp_address((String)o.get("IP_ADDRESS"));
+					uv.setUid((String)o.get("UID"));
+					uv.setUsername((String)o.getString("USERNAME"));
+					vuv.add(uv);					
+				}
+				((HiiMsg)msg).setVector(vuv);
+				return (HiiMsg)msg;
+			}
+			else if(type == "LOG")
+			{
+				((LogMsg)msg).setType("LOG");
+				((LogMsg)msg).setUid((String)obj.get("UID"));
+				((LogMsg)msg).setUsename((String)obj.get("USERNAME"));
+				((LogMsg)msg).setIp_address((String)obj.get("IP_ADDRESS"));
+				return (LogMsg)msg;
+			}
+			else if(type == "MTX")
+			{
+				((MtxMsg)msg).setType("MTX");
+				JSONArray jmatrix, jrow, jvec;
+				Vector<Vector<Knowlage>>  kmac= new Vector ();
+				Vector<Knowlage> kvec;
+				jmatrix = (JSONArray)obj.get("MAC");
+				Knowlage k;
+				Iterator itm = jmatrix.iterator();
+				Iterator itr;
+				while (itm.hasNext())
+				{
+					jrow = (JSONArray)itm.next();
+					itr = jrow.iterator();
+					kvec = new Vector<Knowlage>();
+					while (itr.hasNext());
+					{						
+						k = new Knowlage();
+						k.setMsg_no(((Knowlage)itr.next()).getMsg_no());
+						kvec.add(k);						
+					}
+					kmac.add(kvec);
+				}
+				((MtxMsg)msg).setMac(kmac);
+				Vector<UserId> uidvec = new Vector<UserId>();
+				jvec = (JSONArray) obj.getJSONArray("VEC");
+				Iterator itv = jvec.iterator();
+				UserId uid;
+				Long l;
+				while (itv.hasNext())
+				{
+					uid = new UserId();
+					l = (Long)itv.next(); /////////////// <----------------- sprawdzic
+					uid.setUser_id(l.longValue());
+					uidvec.add(uid);
+				}
+				((MtxMsg)msg).setVector(uidvec);
+				return (MtxMsg) msg;
+			}
+				
+			else
+				return msg;
+			
+		}
+		catch(Exception e)
+		{
+			return msg;
+		}
 	}
 	
-	public Serializer()
-	{
-		obj = new JSONObject();
+	public Serializer()	
+	{		
+		try{
+			obj = new JSONObject();
+			msg = new Message();
+			
+		}
+		catch(Exception e)
+		{
+			System.out.print("Blad tworzenia serializatora");
+		}
+		
 	}
 
 	public JSONObject getObj() {
@@ -149,6 +263,14 @@ public class Serializer {
 
 	public void setObj(JSONObject obj) {
 		this.obj = obj;
+	}
+
+	public Message getMsg() {
+		return msg;
+	}
+
+	public void setMsg(Message msg) {
+		this.msg = msg;
 	}
 	
 }
--- impl/java/src/Test1.java
+++ impl/java/src/Test1.java
@@ -8,9 +8,20 @@ public class Test1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Knowlage k = new Knowlage(1);
+		UserId u = new UserId(2);
+		Vector vk = new Vector();
+		vk.add(k);
+		Vector vu = new Vector();
+		vu.add(u);
+		MsgMsg msg = new MsgMsg("krzysiek", "olka", 1, vk, vu, "zawartosc");
+		Serializer serial =	new Serializer();		
+		serial.pack(msg);
+		System.out.println(serial.getObj().toString());
+		
 		//
 		// TODO Auto-generated method stub
-		Vector <Contact> v = new Vector<Contact>();
+		/*Vector <Contact> v = new Vector<Contact>();
 		v.add(new Contact(1,"krzysiek",true));
 		v.add(new Contact(2,"krzysiek2",true));
 		Contact sasiad = new Contact(1,"k",true);
@@ -18,11 +29,11 @@ public class Test1 {
 		Contact c =new Contact(3,"ja",true);
 		MyId myid = new MyId();
 		GUI Gui = new GUI(v,c,sasiad);
-		
+		*/
 		//myid.findNIC();
 		//System.out.println(myid.findIp(myid.inaddr));
 		//System.out.println(myid.findMac(myid.nic));
-		System.out.println(myid.findId());
+		//System.out.println(myid.findId());
 		//System.out.println(myid.getIp());		
 
 	}
